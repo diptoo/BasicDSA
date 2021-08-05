4cb89ecc34080f5c05c61229f248e68187f3fd2e
dipto <dipto33120@gmail.com>
2021-08-05 17:24:21 +0600

    Implement insertion sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,1 +1,38 @@
-asdasd
+#include <iostream>
+using namespace std;
+
+void fileIO()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int currentItem = arr[i];
+        int prevIndex = i - 1;
+
+        while (prevIndex >= 0 && arr[prevIndex] > currentItem)
+        {
+            arr[prevIndex + 1] = arr[prevIndex];
+            prevIndex--;
+        }
+        arr[prevIndex + 1] = currentItem;
+    }
+}
+
+int main()
+{
+    fileIO();
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    for (auto item : arr)
+    {
+        cout << item << ',';
+    }
+}
